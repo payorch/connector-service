0081de9b5c3e1f2fa5eb8280af6aea761aaa6eb9
Sweta-Kumari-Sharma <ss6175983@gmail.com>
2025-04-10 22:37:09 +0530

    razorpay auth and psync added

---
+++ backend/connector-integration/src/connectors/razorpay.rs
@@ -0,0 +1,290 @@
+pub mod transformers;
+
+use hyperswitch_common_utils::{
+    errors::CustomResult,
+    ext_traits::{ByteSliceExt, OptionExt},
+    request::RequestContent,
+    types::{AmountConvertor, MinorUnit},
+};
+
+use base64::engine::general_purpose::STANDARD;
+use base64::Engine;
+use error_stack::ResultExt;
+use hyperswitch_domain_models::{
+    router_data::{ConnectorAuthType, ErrorResponse},
+    router_data_v2::{flow_common_types::PaymentFlowData, RouterDataV2},
+    router_flow_types::payments::{Authorize, PSync},
+    router_request_types::{PaymentsAuthorizeData, PaymentsSyncData, SyncRequestType},
+    router_response_types::PaymentsResponseData,
+};
+use hyperswitch_interfaces::{
+    api::{self, CaptureSyncMethod, ConnectorCommon},
+    configs::Connectors,
+    connector_integration_v2::ConnectorIntegrationV2,
+    errors,
+    events::connector_api_logs::ConnectorEvent,
+    types::Response,
+};
+use hyperswitch_masking::{Mask, Maskable, PeekInterface};
+
+use transformers::{self as razorpay, ForeignTryFrom};
+
+pub(crate) mod headers {
+    pub(crate) const CONTENT_TYPE: &str = "Content-Type";
+    pub(crate) const AUTHORIZATION: &str = "Authorization";
+}
+
+#[derive(Clone)]
+pub struct Razorpay {
+    _amount_converter: &'static (dyn AmountConvertor<Output = MinorUnit> + Sync),
+}
+
+impl ConnectorCommon for Razorpay {
+    fn id(&self) -> &'static str {
+        "razorpay"
+    }
+    fn get_currency_unit(&self) -> api::CurrencyUnit {
+        api::CurrencyUnit::Minor
+    }
+    fn get_auth_header(
+        &self,
+        auth_type: &ConnectorAuthType,
+    ) -> CustomResult<Vec<(String, Maskable<String>)>, errors::ConnectorError> {
+        let auth = razorpay::RazorpayAuthType::try_from(auth_type)
+            .map_err(|_| errors::ConnectorError::FailedToObtainAuthType)?;
+        let encoded_api_key =
+            STANDARD.encode(format!("{}:{}", auth.key_id.peek(), auth.secret_key.peek()));
+        Ok(vec![(
+            headers::AUTHORIZATION.to_string(),
+            format!("Basic {encoded_api_key}").into_masked(),
+        )])
+    }
+    fn base_url<'a>(&self, connectors: &'a Connectors) -> &'a str {
+        connectors.razorpay.base_url.as_ref()
+    }
+
+    fn build_error_response(
+        &self,
+        res: Response,
+        event_builder: Option<&mut ConnectorEvent>,
+    ) -> CustomResult<ErrorResponse, errors::ConnectorError> {
+        let response: razorpay::RazorpayErrorResponse = res
+            .response
+            .parse_struct("ErrorResponse")
+            .map_err(|_| errors::ConnectorError::ResponseDeserializationFailed)?;
+
+        event_builder.map(|i| i.set_error_response_body(&response));
+
+        Ok(ErrorResponse {
+            status_code: res.status_code,
+            code: response.error_code,
+            message: response.message.to_owned(),
+            reason: Some(response.message),
+            attempt_status: None,
+            connector_transaction_id: response.psp_reference,
+        })
+    }
+}
+
+impl ConnectorIntegrationV2<Authorize, PaymentFlowData, PaymentsAuthorizeData, PaymentsResponseData>
+    for Razorpay
+{
+    fn get_headers(
+        &self,
+        req: &RouterDataV2<Authorize, PaymentFlowData, PaymentsAuthorizeData, PaymentsResponseData>,
+    ) -> CustomResult<Vec<(String, Maskable<String>)>, errors::ConnectorError>
+    where
+        Self: ConnectorIntegrationV2<
+            Authorize,
+            PaymentFlowData,
+            PaymentsAuthorizeData,
+            PaymentsResponseData,
+        >,
+    {
+        let mut header = vec![(
+            headers::CONTENT_TYPE.to_string(),
+            "application/json".to_string().into(),
+        )];
+        let mut api_key = self.get_auth_header(&req.connector_auth_type)?;
+        header.append(&mut api_key);
+        Ok(header)
+    }
+
+    fn get_url(
+        &self,
+        _req: &RouterDataV2<
+            Authorize,
+            PaymentFlowData,
+            PaymentsAuthorizeData,
+            PaymentsResponseData,
+        >,
+    ) -> CustomResult<String, errors::ConnectorError> {
+        Ok(format!(
+            "{}/v1/payments/create/json",
+            "https://checkout-test.razorpay.com/"
+        ))
+    }
+
+    fn get_request_body(
+        &self,
+        req: &RouterDataV2<Authorize, PaymentFlowData, PaymentsAuthorizeData, PaymentsResponseData>,
+    ) -> CustomResult<Option<RequestContent>, errors::ConnectorError> {
+        let connector_router_data =
+            razorpay::RazorpayRouterData::try_from((req.request.minor_amount, req))?;
+        let connector_req = razorpay::RazorpayPaymentRequest::try_from(&connector_router_data)?;
+        Ok(Some(RequestContent::Json(Box::new(connector_req))))
+    }
+
+    fn handle_response_v2(
+        &self,
+        data: &RouterDataV2<
+            Authorize,
+            PaymentFlowData,
+            PaymentsAuthorizeData,
+            PaymentsResponseData,
+        >,
+        event_builder: Option<&mut ConnectorEvent>,
+        res: Response,
+    ) -> CustomResult<
+        RouterDataV2<Authorize, PaymentFlowData, PaymentsAuthorizeData, PaymentsResponseData>,
+        errors::ConnectorError,
+    > {
+        let response: razorpay::RazorpayResponse = res
+            .response
+            .parse_struct("RazorpayPaymentResponse")
+            .map_err(|_| errors::ConnectorError::ResponseDeserializationFailed)?;
+
+        event_builder.map(|i| i.set_response_body(&response));
+        RouterDataV2::foreign_try_from((
+            response,
+            data.clone(),
+            res.status_code,
+            data.request.capture_method,
+            false,
+            data.request.payment_method_type,
+        ))
+        .change_context(errors::ConnectorError::ResponseHandlingFailed)
+    }
+
+    fn get_error_response_v2(
+        &self,
+        res: Response,
+        event_builder: Option<&mut ConnectorEvent>,
+    ) -> CustomResult<ErrorResponse, errors::ConnectorError> {
+        self.build_error_response(res, event_builder)
+    }
+
+    fn get_5xx_error_response(
+        &self,
+        res: Response,
+        event_builder: Option<&mut ConnectorEvent>,
+    ) -> CustomResult<ErrorResponse, errors::ConnectorError> {
+        self.build_error_response(res, event_builder)
+    }
+}
+
+impl ConnectorIntegrationV2<PSync, PaymentFlowData, PaymentsSyncData, PaymentsResponseData>
+    for Razorpay
+{
+    fn get_headers(
+        &self,
+        req: &RouterDataV2<PSync, PaymentFlowData, PaymentsSyncData, PaymentsResponseData>,
+    ) -> CustomResult<Vec<(String, Maskable<String>)>, errors::ConnectorError>
+    where
+        Self: ConnectorIntegrationV2<
+            Authorize,
+            PaymentFlowData,
+            PaymentsAuthorizeData,
+            PaymentsResponseData,
+        >,
+    {
+        let mut header = vec![(
+            headers::CONTENT_TYPE.to_string(),
+            "application/json".to_string().into(),
+        )];
+        let mut api_key = self.get_auth_header(&req.connector_auth_type)?;
+        header.append(&mut api_key);
+        Ok(header)
+    }
+
+    fn get_url(
+        &self,
+        req: &RouterDataV2<PSync, PaymentFlowData, PaymentsSyncData, PaymentsResponseData>,
+    ) -> CustomResult<String, errors::ConnectorError> {
+        let payment_id = req
+            .request
+            .connector_transaction_id
+            .get_connector_transaction_id()
+            .change_context(errors::ConnectorError::RequestEncodingFailed)?;
+        Ok(format!(
+            "{}/v1/payments/{}",
+            "https://api.razorpay.com/", payment_id
+        ))
+    }
+
+    fn get_request_body(
+        &self,
+        req: &RouterDataV2<PSync, PaymentFlowData, PaymentsSyncData, PaymentsResponseData>,
+    ) -> CustomResult<Option<RequestContent>, errors::ConnectorError> {
+        let encoded_data = req
+            .request
+            .encoded_data
+            .clone()
+            .get_required_value("encoded_data")
+            .change_context(errors::ConnectorError::RequestEncodingFailed)?;
+
+        Ok(Some(RequestContent::Json(Box::new(encoded_data))))
+    }
+
+    fn handle_response_v2(
+        &self,
+        data: &RouterDataV2<PSync, PaymentFlowData, PaymentsSyncData, PaymentsResponseData>,
+        event_builder: Option<&mut ConnectorEvent>,
+        res: Response,
+    ) -> CustomResult<
+        RouterDataV2<PSync, PaymentFlowData, PaymentsSyncData, PaymentsResponseData>,
+        errors::ConnectorError,
+    > {
+        let response: razorpay::RazorpayResponse = res
+            .response
+            .parse_struct("RazorpayPaymentResponse")
+            .change_context(errors::ConnectorError::ResponseDeserializationFailed)?;
+
+        event_builder.map(|i| i.set_response_body(&response));
+
+        let is_multiple_capture_sync = match data.request.sync_type {
+            SyncRequestType::MultipleCaptureSync(_) => true,
+            SyncRequestType::SinglePaymentSync => false,
+        };
+        RouterDataV2::foreign_try_from((
+            response,
+            data.clone(),
+            res.status_code,
+            data.request.capture_method,
+            is_multiple_capture_sync,
+            data.request.payment_method_type,
+        ))
+        .change_context(errors::ConnectorError::ResponseHandlingFailed)
+    }
+
+    fn get_error_response_v2(
+        &self,
+        res: Response,
+        event_builder: Option<&mut ConnectorEvent>,
+    ) -> CustomResult<ErrorResponse, errors::ConnectorError> {
+        self.build_error_response(res, event_builder)
+    }
+
+    fn get_multiple_capture_sync_method(
+        &self,
+    ) -> CustomResult<CaptureSyncMethod, errors::ConnectorError> {
+        Ok(CaptureSyncMethod::Individual)
+    }
+    fn get_5xx_error_response(
+        &self,
+        res: Response,
+        event_builder: Option<&mut ConnectorEvent>,
+    ) -> CustomResult<ErrorResponse, errors::ConnectorError> {
+        self.build_error_response(res, event_builder)
+    }
+}
---
+++ backend/connector-integration/src/connectors/razorpay/transformers.rs
@@ -0,0 +1,561 @@
+use std::collections::HashMap;
+
+use hyperswitch_api_models::enums::{self, AttemptStatus};
+
+use hyperswitch_common_utils::{pii::Email, request::Method, types::MinorUnit};
+
+use hyperswitch_domain_models::{
+    payment_method_data::{Card, PaymentMethodData},
+    router_data::{ConnectorAuthType, RouterData},
+    router_data_v2::{PaymentFlowData, RouterDataV2},
+    router_flow_types::Authorize,
+    router_request_types::{PaymentsAuthorizeData, ResponseId},
+    router_response_types::{PaymentsResponseData, RedirectForm},
+};
+use hyperswitch_masking::Secret;
+use serde::{Deserialize, Serialize};
+
+#[derive(Default, Debug, Clone, Serialize, Deserialize)]
+pub enum Currency {
+    #[default]
+    USD,
+    EUR,
+}
+
+#[derive(Default, Debug, Clone, Serialize, Deserialize)]
+pub struct Amount {
+    pub currency: enums::Currency,
+    pub value: MinorUnit,
+}
+
+#[derive(Debug, Clone, Serialize, Deserialize)]
+#[serde(rename_all = "lowercase")]
+pub enum CardBrand {
+    Visa,
+}
+
+#[derive(Debug, PartialEq)]
+pub enum ConnectorError {
+    ParsingFailed,
+    NotImplemented,
+    FailedToObtainAuthType,
+}
+
+#[serde_with::skip_serializing_none]
+#[derive(Debug, Clone, Serialize, Deserialize)]
+#[serde(rename_all = "camelCase")]
+pub struct RazorpayCard {
+    number: String,
+    expiry_month: Secret<String>,
+    expiry_year: Secret<String>,
+    cvc: Option<Secret<String>>,
+    holder_name: Option<Secret<String>>,
+    brand: Option<CardBrand>,
+    network_payment_reference: Option<Secret<String>>,
+}
+
+#[derive(Debug, Clone, Serialize)]
+#[serde(tag = "type")]
+#[serde(rename_all = "lowercase")]
+pub enum RazorpayPaymentMethod {
+    #[serde(rename = "scheme")]
+    RazorpayCard(Box<RazorpayCard>),
+}
+
+#[derive(Clone, Default, Debug, Serialize, Deserialize)]
+pub enum AuthType {
+    #[default]
+    PreAuth,
+}
+
+#[serde_with::skip_serializing_none]
+#[derive(Default, Debug, Serialize, Deserialize)]
+#[serde(rename_all = "camelCase")]
+pub struct Address {
+    city: String,
+    country: enums::CountryAlpha2,
+    house_number_or_name: Secret<String>,
+    postal_code: Secret<String>,
+    state_or_province: Option<Secret<String>>,
+    street: Option<Secret<String>>,
+}
+
+#[derive(Debug, Clone, Serialize)]
+#[serde(untagged)]
+pub enum PaymentMethod {
+    RazorpayPaymentMethod(Box<RazorpayPaymentMethod>),
+}
+
+#[derive(Default, Debug, Serialize, Deserialize)]
+#[serde(rename_all = "camelCase")]
+pub struct CardDetails {
+    pub number: String,
+    pub name: String,
+    pub expiry_month: Secret<String>,
+    pub expiry_year: String,
+    pub cvv: Option<Secret<String>>,
+}
+
+#[derive(Default, Debug, Serialize)]
+#[serde(rename_all = "camelCase")]
+pub struct AuthenticationDetails {
+    pub authentication_channel: String,
+}
+
+#[derive(Default, Debug, Serialize)]
+#[serde(rename_all = "camelCase")]
+pub struct BrowserInfo {
+    pub java_enabled: bool,
+    pub javascript_enabled: bool,
+    pub timezone_offset: i32,
+    pub color_depth: i32,
+    pub screen_width: i32,
+    pub screen_height: i32,
+    pub language: String,
+}
+
+#[derive(Debug, Serialize)]
+#[serde(rename_all = "camelCase")]
+pub struct RazorpayPaymentRequest {
+    pub amount: MinorUnit,
+    pub currency: String,
+    pub contact: String,
+    pub email: Email,
+    pub order_id: String,
+    pub method: String,
+    #[serde(flatten)]
+    pub payment_method_data: PaymentMethodSpecificData,
+    pub authentication: Option<AuthenticationDetails>,
+    pub browser: BrowserInfo,
+    pub ip: String,
+    pub referer: String,
+    pub user_agent: String,
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+#[serde(tag = "method", rename_all = "snake_case")]
+pub enum PaymentMethodSpecificData {
+    Card(CardDetails),
+}
+
+#[derive(Debug, Clone, Serialize, Deserialize)]
+#[serde(rename_all = "lowercase")]
+pub enum PaymentMethodType {
+    Card,
+    Wallet,
+    Upi,
+    Emi,
+    Netbanking,
+}
+
+#[derive(Debug, Serialize)]
+pub struct RazorpayRouterData<T> {
+    pub amount: MinorUnit,
+    pub router_data: T,
+}
+
+impl<T> TryFrom<(MinorUnit, T)> for RazorpayRouterData<T> {
+    type Error = hyperswitch_interfaces::errors::ConnectorError;
+    fn try_from((amount, item): (MinorUnit, T)) -> Result<Self, Self::Error> {
+        Ok(Self {
+            amount,
+            router_data: item,
+        })
+    }
+}
+
+pub struct RazorpayAuthType {
+    pub(super) key_id: Secret<String>,
+    pub(super) secret_key: Secret<String>,
+}
+
+impl TryFrom<&ConnectorAuthType> for RazorpayAuthType {
+    type Error = hyperswitch_interfaces::errors::ConnectorError;
+    fn try_from(auth_type: &ConnectorAuthType) -> Result<Self, Self::Error> {
+        match auth_type {
+            ConnectorAuthType::BodyKey { api_key, key1 } => Ok(Self {
+                key_id: api_key.to_owned(),
+                secret_key: key1.to_owned(),
+            }),
+            _ => Err(hyperswitch_interfaces::errors::ConnectorError::FailedToObtainAuthType),
+        }
+    }
+}
+
+impl TryFrom<(&Card, Option<Secret<String>>)> for RazorpayPaymentMethod {
+    type Error = ConnectorError;
+    fn try_from(
+        (card, card_holder_name): (&Card, Option<Secret<String>>),
+    ) -> Result<Self, Self::Error> {
+        let razorpay_card = RazorpayCard {
+            number: card.card_number.clone().to_string(),
+            expiry_month: card.card_exp_month.clone(),
+            expiry_year: "2031".to_string().into(),
+            cvc: Some(card.card_cvc.clone()),
+            holder_name: card_holder_name,
+            brand: Some(CardBrand::Visa),
+            network_payment_reference: None,
+        };
+        Ok(RazorpayPaymentMethod::RazorpayCard(Box::new(razorpay_card)))
+    }
+}
+
+impl
+    TryFrom<(
+        &RazorpayRouterData<
+            &RouterDataV2<Authorize, PaymentFlowData, PaymentsAuthorizeData, PaymentsResponseData>,
+        >,
+        &Card,
+    )> for RazorpayPaymentRequest
+{
+    type Error = hyperswitch_interfaces::errors::ConnectorError;
+
+    fn try_from(
+        value: (
+            &RazorpayRouterData<
+                &RouterDataV2<
+                    Authorize,
+                    PaymentFlowData,
+                    PaymentsAuthorizeData,
+                    PaymentsResponseData,
+                >,
+            >,
+            &Card,
+        ),
+    ) -> Result<Self, Self::Error> {
+        let (item, card_data) = value;
+
+        let amount = item.amount;
+        let currency = item.router_data.request.currency.to_string();
+
+        let contact = "9999999999".to_string();
+
+        let email = item.router_data.request.email.clone().ok_or(
+            hyperswitch_interfaces::errors::ConnectorError::MissingRequiredField {
+                field_name: "email",
+            },
+        )?;
+
+        let order_id = "order_id_here".to_string();
+        let method = "card".to_string();
+        let card_holder_name = "Sweta Sharma".to_string().into();
+        let payment_method_data = PaymentMethodSpecificData::Card(CardDetails {
+            number: card_data.card_number.clone().to_string(),
+            name: card_holder_name,
+            expiry_month: card_data.card_exp_month.clone(),
+            expiry_year: "2030".to_string().into(),
+            cvv: Some(card_data.card_cvc.clone()),
+        });
+
+        let authentication = Some(AuthenticationDetails {
+            authentication_channel: "browser".to_string(),
+        });
+
+        let browser = BrowserInfo {
+            java_enabled: false,
+            javascript_enabled: false,
+            timezone_offset: 0,
+            color_depth: 24,
+            screen_width: 1920,
+            screen_height: 1080,
+            language: "en-US".to_string(),
+        };
+
+        Ok(RazorpayPaymentRequest {
+            amount,
+            currency,
+            contact,
+            email,
+            order_id,
+            method,
+            payment_method_data,
+            authentication,
+            browser,
+            ip: "105.106.107.108".to_string(),
+            referer: "https://merchansite.com/example/paybill".to_string(),
+            user_agent: "Mozilla/5.0".to_string(),
+        })
+    }
+}
+
+impl
+    TryFrom<
+        &RazorpayRouterData<
+            &RouterDataV2<Authorize, PaymentFlowData, PaymentsAuthorizeData, PaymentsResponseData>,
+        >,
+    > for RazorpayPaymentRequest
+{
+    type Error = hyperswitch_interfaces::errors::ConnectorError;
+
+    fn try_from(
+        item: &RazorpayRouterData<
+            &RouterDataV2<Authorize, PaymentFlowData, PaymentsAuthorizeData, PaymentsResponseData>,
+        >,
+    ) -> Result<Self, Self::Error> {
+        match &item.router_data.request.payment_method_data {
+            PaymentMethodData::Card(card) => RazorpayPaymentRequest::try_from((item, card)),
+            _ => Err(
+                hyperswitch_interfaces::errors::ConnectorError::NotImplemented(
+                    "Only card payments are supported".into(),
+                ),
+            ),
+        }
+    }
+}
+
+pub struct ResponseRouterData<Flow, R, Request, Response> {
+    pub response: R,
+    pub data: RouterData<Flow, Request, Response>,
+    pub http_code: u16,
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+#[serde(rename_all = "camelCase")]
+pub struct RazorpayPaymentResponse {
+    pub razorpay_payment_id: String,
+    pub next: Option<Vec<NextAction>>,
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+#[serde(rename_all = "camelCase")]
+pub struct NextAction {
+    pub action: String,
+    pub url: String,
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+#[serde(rename_all = "camelCase")]
+pub enum RazorpayResponse {
+    PaymentResponse(RazorpayPaymentResponse),
+    PsyncResponse(RazorpayPsyncResponse),
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+#[serde(rename_all = "camelCase")]
+pub struct RazorpayPsyncResponse {
+    pub razorpay_payment_id: String,
+    pub entity: String,
+    pub amount: i64,
+    pub currency: String,
+    pub status: RazorpayStatus,
+    pub method: String,
+    pub order_id: Option<String>,
+    pub description: Option<String>,
+    pub international: bool,
+    pub refund_status: Option<String>,
+    pub amount_refunded: i64,
+    pub captured: bool,
+    pub email: String,
+    pub contact: String,
+    pub fee: i64,
+    pub tax: i64,
+    pub error_code: Option<String>,
+    pub error_description: Option<String>,
+    pub error_source: Option<String>,
+    pub error_step: Option<String>,
+    pub error_reason: Option<String>,
+    pub notes: Option<HashMap<String, String>>,
+    pub created_at: i64,
+    pub card_id: Option<String>,
+    pub card: Option<SyncCardDetails>,
+    pub upi: Option<SyncUPIDetails>,
+    pub acquirer_data: Option<Vec<AcquirerData>>,
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+#[serde(rename_all = "camelCase")]
+pub struct SyncCardDetails {
+    pub id: String,
+    pub entity: String,
+    pub name: String,
+    pub last4: i32,
+    pub network: String,
+    pub r#type: String,
+    pub issuer: Option<String>,
+    pub emi: bool,
+    pub sub_type: String,
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+#[serde(rename_all = "camelCase")]
+pub struct SyncUPIDetails {
+    pub payer_account_type: String,
+    pub vpa: String,
+    pub flow: String,
+    pub bank: String,
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+#[serde(rename_all = "camelCase")]
+pub struct AcquirerData {
+    pub rrn: String,
+    pub authentication_reference_number: String,
+    pub bank_transaction_id: String,
+}
+
+#[derive(Debug, Clone, Serialize, Deserialize)]
+pub enum RazorpayStatus {
+    Created,
+    Authorized,
+    Captured,
+    Refunded,
+    Failed,
+}
+
+#[derive(Default, Debug, Serialize, Deserialize)]
+#[serde(rename_all = "snake_case")]
+pub enum CaptureMethod {
+    #[default]
+    Automatic,
+    Manual,
+    ManualMultiple,
+    Scheduled,
+    SequentialAutomatic,
+}
+
+pub trait ForeignTryFrom<F>: Sized {
+    type Error;
+
+    fn foreign_try_from(from: F) -> Result<Self, Self::Error>;
+}
+
+fn get_authorization_razorpay_payment_status_from_action(
+    is_manual_capture: bool,
+    has_next_action: bool,
+) -> AttemptStatus {
+    if has_next_action {
+        AttemptStatus::Charged
+    } else if is_manual_capture {
+        AttemptStatus::Authorized
+    } else {
+        AttemptStatus::Charged
+    }
+}
+
+fn get_psync_razorpay_payment_status(
+    is_manual_capture: bool,
+    razorpay_status: RazorpayStatus,
+) -> AttemptStatus {
+    match razorpay_status {
+        RazorpayStatus::Created => AttemptStatus::Pending,
+        RazorpayStatus::Authorized => {
+            if is_manual_capture {
+                AttemptStatus::Authorized
+            } else {
+                AttemptStatus::Charged
+            }
+        }
+        RazorpayStatus::Captured => AttemptStatus::Charged,
+        RazorpayStatus::Refunded => AttemptStatus::AutoRefunded,
+        RazorpayStatus::Failed => AttemptStatus::Failure,
+    }
+}
+
+impl<F, Req>
+    ForeignTryFrom<(
+        RazorpayResponse,
+        RouterDataV2<F, PaymentFlowData, Req, PaymentsResponseData>,
+        u16,
+        Option<hyperswitch_api_models::enums::CaptureMethod>,
+        bool,
+        Option<hyperswitch_api_models::enums::PaymentMethodType>,
+    )> for RouterDataV2<F, PaymentFlowData, Req, PaymentsResponseData>
+{
+    type Error = hyperswitch_interfaces::errors::ConnectorError;
+
+    fn foreign_try_from(
+        (response, data, _http_code, _capture_method, _is_multiple_capture_psync_flow, _pmt): (
+            RazorpayResponse,
+            RouterDataV2<F, PaymentFlowData, Req, PaymentsResponseData>,
+            u16,
+            Option<hyperswitch_api_models::enums::CaptureMethod>,
+            bool,
+            Option<hyperswitch_api_models::enums::PaymentMethodType>,
+        ),
+    ) -> Result<Self, Self::Error> {
+        let is_manual_capture = false;
+
+        match response {
+            RazorpayResponse::PaymentResponse(payment_response) => {
+                let _status =
+                    get_authorization_razorpay_payment_status_from_action(is_manual_capture, true);
+                let redirect_url = payment_response
+                    .next
+                    .as_ref()
+                    .and_then(|next_actions| next_actions.first())
+                    .map(|action| action.url.clone())
+                    .ok_or_else(|| {
+                        hyperswitch_interfaces::errors::ConnectorError::MissingRequiredField {
+                            field_name: "next.url",
+                        }
+                    })?;
+
+                let mut form_fields = HashMap::new();
+                form_fields.insert(
+                    "transaction_id".to_string(),
+                    payment_response.razorpay_payment_id.clone(),
+                );
+                if let Some(next_action) = payment_response.next {
+                    for action in next_action {
+                        form_fields.insert("action".to_string(), action.action);
+                        form_fields.insert("url".to_string(), action.url);
+                    }
+                }
+                let payment_response_data = PaymentsResponseData::TransactionResponse {
+                    resource_id: ResponseId::ConnectorTransactionId(
+                        payment_response.razorpay_payment_id.clone(),
+                    ),
+                    redirection_data: *Box::new(Some(RedirectForm::Form {
+                        endpoint: redirect_url,
+                        method: Method::Post,
+                        form_fields,
+                    })),
+                    mandate_reference: None,
+                    connector_metadata: None,
+                    network_txn_id: None,
+                    connector_response_reference_id: None,
+                    incremental_authorization_allowed: None,
+                    charge_id: None,
+                };
+                let error = None;
+
+                Ok(Self {
+                    response: error.map_or_else(|| Ok(payment_response_data), Err),
+                    ..data
+                })
+            }
+            RazorpayResponse::PsyncResponse(psync_response) => {
+                let _status =
+                    get_psync_razorpay_payment_status(is_manual_capture, psync_response.status);
+                let psync_response_data = PaymentsResponseData::TransactionResponse {
+                    resource_id: ResponseId::ConnectorTransactionId(
+                        psync_response.razorpay_payment_id,
+                    ),
+                    redirection_data: None,
+                    mandate_reference: None,
+                    connector_metadata: None,
+                    network_txn_id: None,
+                    connector_response_reference_id: None,
+                    incremental_authorization_allowed: None,
+                    charge_id: None,
+                };
+                let error = None;
+
+                Ok(Self {
+                    response: error.map_or_else(|| Ok(psync_response_data), Err),
+                    ..data
+                })
+            }
+        }
+    }
+}
+
+#[derive(Debug, Default, Serialize, Deserialize)]
+#[serde(rename_all = "camelCase")]
+pub struct RazorpayErrorResponse {
+    pub status: i32,
+    pub error_code: String,
+    pub message: String,
+    pub error_type: String,
+    pub psp_reference: Option<String>,
+}
